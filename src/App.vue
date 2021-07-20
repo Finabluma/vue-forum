ab77ef9fb152364970234c44b38f5cb850b516f8
finabluma <oscar.francino@finabluma.com>
2021-07-20 22:20:28 +0200

    router view

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <script>
-import PageHome from "@/components/PageHome.vue";
 
 export default {
   name: "App",
-  components: {
-    PageHome,
-  },
 };
 </script>
 
 <style>
-@import "assets/style.css";
+@import "./assets/style.css";
 /* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,6 +20,6 @@ export default {
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <div class="container">
-    <page-home />
+    <router-view />
   </div>
 </template>

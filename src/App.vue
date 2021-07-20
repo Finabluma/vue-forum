4f7707d299145a1fcc1f12cb942e90436fbc9e43
finabluma <oscar.francino@finabluma.com>
2021-07-20 21:38:18 +0200

    application structure

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHome from "@/components/PageHome.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PageHome,
+  },
+};
 </script>
 
 <style>
-#app {
+@import "assets/style.css";
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="container">
+    <page-home />
+  </div>
+</template>
